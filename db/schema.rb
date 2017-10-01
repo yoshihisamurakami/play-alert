dc7e54b7576895218db03586f385db616cc479f1
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2017-10-01 21:27:18 +0900

    Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921060348) do
+ActiveRecord::Schema.define(version: 20171001122411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170921060348) do
     t.string "theater"
     t.date "startdate"
     t.date "enddate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.boolean "regular"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
