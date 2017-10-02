9f1ef6e1e41711b1553069e706185d9904b04c5e
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2017-10-02 22:04:14 +0900

    userモデル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :email,     presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      email: "test@example.com", 
+      regular: false,
+    )
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "email がなければエラー" do
+    @user.email = '  '
+    assert_not @user.valid?
+  end
+  
+  test "email が長すぎないこと" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
