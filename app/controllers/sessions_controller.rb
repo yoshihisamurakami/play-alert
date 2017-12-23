76b8990dd0b36f31ab8ef83755a1f7739c2624ac
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2017-12-23 21:28:39 +0900

    ログイン・ログアウト（途中まで）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # OK
       log_in user
       redirect_to root_path
     else
-      # ERROR
       flash.now[:danger] = "ユーザーが存在しないか、ユーザー名とパスワードの組み合わせが正しくありません。"
       render 'new'
     end
   end
   
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:yoshihisa)
+  end
+
   test "無効なユーザーIDでログイン" do
     get login_path
     assert_template 'sessions/new'
@@ -10,5 +14,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "有効なユーザーでログイン" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
   end
 end
