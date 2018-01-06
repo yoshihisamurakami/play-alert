6d9fe224e2a5b9779bb2c6f2d21f286084a1deaf
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2018-01-06 16:16:44 +0900

    カレンダーで公演をクリックしたら公演情報に飛ぶように変更

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -8,6 +8,7 @@ class CalendarController < ApplicationController
         'title' => stage.group + " " + stage.title,
         'start' => stage.startdate.to_s + " 00:00:00",
         'end' => stage.enddate.to_s + " 23:59:59",
+        'url' => stage_path(stage.id),
       ]
     end
   end
