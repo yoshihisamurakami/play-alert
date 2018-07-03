3657d85018150e494a72f64a398bb1425d10aed1
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2018-07-04 07:10:48 +0900

    舞台詳細情報取得バッチでログ出力が多すぎたので修正

--- app/helpers/get_stage_details_helper.rb
+++ app/helpers/get_stage_details_helper.rb
@@ -16,32 +16,61 @@ module GetStageDetailsHelper
   def get_stage_details
     puts "get_stage_details helper start!!"
     msg = ''
-    stages = Stage.all
-    count=0
-    stages.each do |stage|
-      next unless need_update?(stage.id)
-      puts stage.id.to_s + " " + stage.title + " " + stage.url
-      msg += stage.id.to_s + " " + stage.title + " " + stage.url + "\r\n"
-
-      html = get_html(CORICH_URL_DOMAIN + stage.url)
-      if html[:status] == NOT_FOUND
-        puts "stage-page not found."
-        stage.destroy
+    @update_count = 0
+    @new_rec = 0
+    @updated = 0
+    stages = Stage.pluck(:id)
+    details = StageDetail.pluck(:stage_id)
+    
+    # stage レコードがあり、stage_detail にレコードがないデータ
+    new_stages = stages - details
+    new_stages.sort.each do |stage_id|
+      stage = Stage.find(stage_id)
+      if update_detail stage
+        @new_rec += 1
+        @update_count += 1
+        break if @update_count >= 100
       end
-      next if !html[:html]
-      
-      detail = get_detailinfo(html[:html], html[:charset])
-      save_detail(stage.id, detail)
-      count += 1
-      break if count >= 100
-      sleep 2
     end
-    puts "更新件数 => " + count.to_s + "\r\n"
-    msg += "更新件数 => " + count.to_s + "\r\n"
-    #if count > 0
-    #  StageMailer.getstages_message(msg).deliver_now
-    #end
-    return
+
+    if @update_count < 100
+      #stage_detailsテーブルで
+      stages = Stage.includes(:stage_detail)
+        .where("stage_details.updated_at < ?", 3.days.ago)
+        .references(:stage_detail)
+      stages.each do |stage|
+        if update_detail stage
+          @updated += 1
+          @update_count += 1
+          break if @update_count >= 100
+        end
+      end
+    end
+    
+    puts "更新件数 => " + @update_count.to_s + "\r\n"
+    if (@new_rec + @updated > 0) 
+      msg = "新規 : #{@new_rec}  更新 : #{@updated}"
+      p msg
+      StageMailer.getstages_message(msg).deliver_now
+    end
+  end
+
+  def update_detail(stage)
+    puts stage.id.to_s + " " + stage.title + " " + stage.url
+    
+    html = get_html(CORICH_URL_DOMAIN + stage.url)
+    if html[:status] == NOT_FOUND
+      puts "stage-page not found."
+      stage.destroy
+      return false
+    end
+    return false if !html[:html]
+
+    detail = get_detailinfo(html[:html], html[:charset])
+    save_detail(stage.id, detail)
+
+    sleep 2
+    true
   end
 
   def get_html(url)
@@ -52,13 +81,13 @@ module GetStageDetailsHelper
         f.read
       end
     rescue => e
+      return {status: NOT_FOUND} if e.to_s.match(/Not Found/)
+      
       msg = "エラークラス -> #{e.class}\r\n"
       msg += "エラーメッセージ -> #{e.message}\r\n"
       msg += "バックトレース -> \r\n"
       msg += "#{e.backtrace}"
       StageMailer.getstages_message(msg).deliver_now
-      
-      return {status: NOT_FOUND} if e.to_s.match(/Not Found/)
       return nil
     end
     {html: html, charset: charset} 
